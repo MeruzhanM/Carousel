dcd3c44f9b711dc7e832a62038088383fdb9fb90
Meruzhan <160716306+MeruzhanM@users.noreply.github.com>
2024-04-14 16:59:23 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const images = document.querySelectorAll('img');
+let counter = 0;
+const btnRight = document.querySelector('.btn-right');
+const btnLeft = document.querySelector('.btn-left');
+btnRight.addEventListener('click', function() {
+    images[counter].style.display = 'none'
+    counter = (counter + 1) % images.length;
+    images[counter].style.display = 'block';
+})
+
+btnLeft.addEventListener('click', function() {
+    images[counter].style.display = 'none';
+    counter = (counter - 1 + images.length) % images.length;
+    images[counter].style.display = 'block';
+});
